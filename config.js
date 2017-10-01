bfa74fbe9a1e4443f5be621c5447dfe4b72ddbfc
Tony Brancato <tonybrancato1@gmail.com>
2017-09-30 21:55:18 -0400

    changed database url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/restaurants-app';
+                      'mongodb://localhost/gamekeeper';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-restaurants-app');
+	'mongodb://localhost/test-gamekeeper');
 exports.PORT = process.env.PORT || 8080;
