b165f6e3183da9f23de9ab51a7f7d64fd03d5a11
Tony Brancato <tonybrancato1@gmail.com>
2017-10-03 21:28:20 -0400

    corrected GET url

--- test/games-tests-ci.js
+++ test/games-tests-ci.js
@@ -58,7 +58,7 @@ describe('GET endpoint', function() {
 
       let res;
       return chai.request(app)
-        .get('/board-games')
+        .get('/api-board-games')
         .then(function(_res) {
           res = _res;
           res.should.have.status(200);
